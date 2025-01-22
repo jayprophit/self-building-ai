ad26d7a394a896580515445739bf46df24d5f4e9
Jonathan Powell <jprophit@gmail.com>
2025-01-22 11:28:39 +0000

    update

---
+++ private/models/ai_model.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+
+def create_model():
+    model = tf.keras.Sequential([
+        tf.keras.layers.Dense(64, activation='relu', input_shape=(10,)),
+        tf.keras.layers.Dense(1, activation='sigmoid')
+    ])
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    return model
+
+if __name__ == "__main__":
+    model = create_model()
+    model.summary()
